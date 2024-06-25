d30717d5679e448d012eb7fe24ce571cf2d7b1d4
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-25 11:55:18 +0300

    created profile page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>The about page inludes some information about my current project.</p>
+  </div>
+
+  <div class="container">
+    <p>Second paragraph.</p>
   </div>
 </template>
 
 <style>
+.about, .container {
+  color: bisque;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 4em;
+  margin-top: .7rem;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps ({
+    user: {
+        type: String,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <header class="header">
+        <p>This is a User's Profile Page.</p>
+    </header>
+    <main class="main">
+        <div class="wrapper">
+            <div class="content">
+                <p>{{user}}</p>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style>
+    .header {
+        color: bisque;
+    }
+</style>
